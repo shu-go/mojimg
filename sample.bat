20195e7561a4904bc31149fe629bfd4c91970a6e
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2015-06-13 14:36:33 +0900

    more sample commandlines in sample.bat

--- sample.bat
+++ sample.bat
@@ -1,3 +1,13 @@
+@echo off
+rem expecting path:
+rem ./font/
+rem    ipagmr.ttf
+rem mojimg
+rem sample.bat (this file)
+@echo on
+
 mojimg -bg 0ff -pos lt left top | mojimg -pos ct center top | mojimg -pos rt right top | mojimg -pos lm left middle | mojimg -fg f00 -pos cm center middle | mojimg -pos rm right middle | mojimg -pos lb left bottom | mojimg -pos cb center bottom | mojimg -pos rb right bottom > sample.png
 type sample.png | mojimg -type=jpg -pos rb " "  > sample.jpg
-pause
+
+mojimg -output sample_autodetection.jpg JPEG
+mojimg -output sample_autodetection.png PNG
